30d21118c1b647afccdd3bd30f10addf58d4d411
Flavio-Lisboa <flavio.fernandes1200@gmail.com>
2023-06-05 16:56:03 -0300

    Finishing JwtAuthenticationFilter

--- src/main/java/com/flavio/flashfeast/domain/config/JwtAuthenticationFilter.java
+++ src/main/java/com/flavio/flashfeast/domain/config/JwtAuthenticationFilter.java
@@ -1,5 +1,10 @@
 package com.flavio.flashfeast.domain.config;
 
+import com.flavio.flashfeast.domain.entities.Company;
+import com.flavio.flashfeast.domain.entities.User;
+import com.flavio.flashfeast.domain.exception.NotFoundException;
+import com.flavio.flashfeast.domain.repository.CompanyRepository;
+import com.flavio.flashfeast.domain.repository.UserRepository;
 import com.flavio.flashfeast.domain.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,15 +12,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
@@ -29,5 +41,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         entityEmail = jwtService.extractEntityEmail(jwt);
+        if(entityEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            Optional<User> user = userRepository.findByEmail(entityEmail);
+            Optional<Company> company = companyRepository.findByEmail(entityEmail);
+            UserDetails userDetails;
+
+            if(user.isPresent()) userDetails = user.get();
+            else if(company.isPresent()) userDetails = company.get();
+            else throw new NotFoundException("Entity Not Found");
+
+            if(jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/flavio/flashfeast/domain/entities/Company.java
+++ src/main/java/com/flavio/flashfeast/domain/entities/Company.java
@@ -3,6 +3,11 @@ package com.flavio.flashfeast.domain.entities;
 import com.flavio.flashfeast.domain.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor
@@ -11,7 +16,7 @@ import lombok.*;
 @Builder
 @Entity
 @Table(name = "company_table")
-public class Company {
+public class Company implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +31,34 @@ public class Company {
     @Enumerated(EnumType.STRING)
     private Role role;
     private String logo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
