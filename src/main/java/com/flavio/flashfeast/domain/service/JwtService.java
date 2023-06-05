555ee2f8c9839e63e05e6ddff740b98fd1c986f1
Flavio-Lisboa <flavio.fernandes1200@gmail.com>
2023-06-04 21:19:38 -0300

    Extracting a specific claim

---
+++ src/main/java/com/flavio/flashfeast/domain/service/JwtService.java
@@ -0,0 +1,41 @@
+package com.flavio.flashfeast.domain.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secretKey}")
+    private String SECRET_KEY;
+
+    public String extractEntityEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
